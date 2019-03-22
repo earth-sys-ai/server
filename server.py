c119728a7a2e5b8fcc86310b417391691a7d958d
Michael Bednarek <mbednarek360@protonmail.com>
2019-03-21 22:19:07 -0400

    Initial Commit

---
+++ server.py
@@ -0,0 +1,40 @@
+# import flask modules
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_cors import CORS
+
+# data retrieval code
+import sys
+sys.path.append('database')
+from data import *
+
+# get location from query string
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def display():
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    add = request.args.get('add')
+    clr = request.args.get('clr')
+    red = request.args.get('red')
+    lis = request.args.get('lis')
+    
+    if (red == "true"):
+        return getData(lat, lng)
+    
+    elif (clr == "true"):
+        clearData(lat, lng)
+        return ""
+
+    elif (lis == "true"):
+        return listData()
+
+    elif(add != ""):
+        addData(lat, lng, add)
+        return ""
+
+# begin web server
+if __name__=='__main__':
+    app.run(port=5000)
